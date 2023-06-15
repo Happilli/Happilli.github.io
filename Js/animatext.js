f50bd7f34bb088d0739bc0f1c063036dd341bae5
Safal Lama <133653188+Happilli@users.noreply.github.com>
2023-06-15 12:54:09 +0545

    Update animatext.js

--- Js/animatext.js
+++ Js/animatext.js
@@ -1,7 +1,5 @@
 var typed = new Typed('#element', {
     strings: ['Student','Video Editor','Freelancer','Otaku','Fanboy','Introvert'],
-    typeSpeed: 10,
-    backSpeed: 10,
+    typeSpeed: 25,
     loop: true,
-    shuffle: true
   });
