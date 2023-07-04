c9f55de931c8d29ed36e6fed356dc96e6d720077
Safal Lama <133653188+Happilli@users.noreply.github.com>
2023-07-04 12:08:15 +0545

    prototyping

---
+++ Js/extra.js
@@ -0,0 +1,33 @@
+  var typed = new Typed('#typed', {
+    strings: ['Hi there!', 'This is Safal\'s personal server.'],
+    typeSpeed: 40,
+    backSpeed: 30,
+    loop: true
+  });
+
+  function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+  }
+
+  function toggleMenu() {
+    var menuIcon = document.querySelector('.menu-icon');
+    var projectsContainer = document.querySelector('.projects-container');
+
+    menuIcon.classList.toggle('open');
+    projectsContainer.classList.toggle('open');
+  }
+
+  // Add a click event listener to the document
+  document.addEventListener('click', function(event) {
+    var menuIcon = document.querySelector('.menu-icon');
+    var projectsContainer = document.querySelector('.projects-container');
+
+    // Check if the projects container is open
+    if (projectsContainer.classList.contains('open')) {
+      // Check if the clicked element is outside the projects container and menu icon
+      if (!projectsContainer.contains(event.target) && !menuIcon.contains(event.target)) {
+        menuIcon.classList.remove('open');
+        projectsContainer.classList.remove('open');
+      }
+    }
+  });
