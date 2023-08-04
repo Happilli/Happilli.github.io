476d99f51f4b671531d14bc06a34b663012bdd02
happilli <safalisgod@gmail.com>
2023-08-04 17:51:01 +0545

    projectSectionchanges

--- Js/theme.js
+++ Js/theme.js
@@ -1,22 +1,36 @@
 function toggleTheme() {
   var body = document.getElementsByTagName('body')[0];
   var themeIcon = document.getElementsByClassName('theme-icon')[0];
-  
+
   body.classList.toggle('dark-theme');
   themeIcon.classList.toggle('dark');
 
-  // Save the current theme preference in localStorage
   if (body.classList.contains('dark-theme')) {
       localStorage.setItem('theme', 'dark');
   } else {
       localStorage.setItem('theme', 'light');
   }
+
+  if (body.classList.contains('dark-theme')) {
+      body.classList.remove('light-theme');
+      themeIcon.style.borderColor = 'white'; // Set white border in night mode
+  } else {
+      body.classList.add('light-theme');
+      themeIcon.style.borderColor = 'black'; // Set black border in light mode
+  }
 }
 
-// Check if a theme preference is saved in localStorage and apply it on page load
 document.addEventListener('DOMContentLoaded', function () {
   var savedTheme = localStorage.getItem('theme');
+  var body = document.getElementsByTagName('body')[0];
+  var themeIcon = document.getElementsByClassName('theme-icon')[0];
+
   if (savedTheme === 'dark') {
       toggleTheme();
+  }
+
+  if (savedTheme === 'light') {
+      body.classList.add('light-theme');
+      themeIcon.style.borderColor = 'black'; // Set black border in light mode
   }
 });
