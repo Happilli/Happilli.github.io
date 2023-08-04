58a2b76ee3b4e47747de97a8a2371f2265f42328
happilli <safalisgod@gmail.com>
2023-08-04 16:22:03 +0545

    appliedChangesMajor

--- Js/theme.js
+++ Js/theme.js
@@ -1,7 +1,22 @@
 function toggleTheme() {
-    var body = document.getElementsByTagName('body')[0];
-    var themeIcon = document.getElementsByClassName('theme-icon')[0];
-    
-    body.classList.toggle('dark-theme');
-    themeIcon.classList.toggle('dark');
+  var body = document.getElementsByTagName('body')[0];
+  var themeIcon = document.getElementsByClassName('theme-icon')[0];
+  
+  body.classList.toggle('dark-theme');
+  themeIcon.classList.toggle('dark');
+
+  // Save the current theme preference in localStorage
+  if (body.classList.contains('dark-theme')) {
+      localStorage.setItem('theme', 'dark');
+  } else {
+      localStorage.setItem('theme', 'light');
   }
+}
+
+// Check if a theme preference is saved in localStorage and apply it on page load
+document.addEventListener('DOMContentLoaded', function () {
+  var savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+      toggleTheme();
+  }
+});
