bdffef9668668236c38a564e4bb14d58ebdb604c
happilli <safalisgod@gmail.com>
2023-08-05 12:52:20 +0545

    hamFix

--- Js/resp.js
+++ Js/resp.js
@@ -1,13 +1,11 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-rightNav = document.querySelector('.right-nav')
-navlist= document.querySelector('.nav-list')
- 
-burger.addEventListener('click', ()=>{
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+rightNav = document.querySelector('.right-nav');
+navlist = document.querySelector('.nav-list');
+
+burger.addEventListener('click', () => {
     rightNav.classList.toggle('v-class-resp');
     navlist.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
-})
-
-
-
+    burger.classList.toggle('toggle');
+});
